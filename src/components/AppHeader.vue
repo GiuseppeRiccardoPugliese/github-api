f2bb8ad63622ebe47e15cad166e9a35fd456d380
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-04-09 19:55:25 +0200

    aggiunta ricerca nell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,10 +19,15 @@ export default {
     <nav class="navbar bg-light">
         <div class="container-fluid">
             <a class="navbar-brand"><strong>GitHub Api</strong></a>
-            <select name="seleziona" id="">
-                <option value="Seleziona" selected disabled>Seleziona un'opzione</option>
-                <option value="Repositories" aria-placeholder="Inserisci una tipologia">Repositories</option>
-            </select>
+            <div class="search_input d-flex">
+                <input class="form-control mx-1" type="search" placeholder="Search" aria-label="Search">
+                <select class="mx-1" name="seleziona" id="">
+                    <option value="Seleziona" selected disabled>Seleziona un'opzione</option>
+                    <option value="Repositories" aria-placeholder="Inserisci una tipologia">Repositories</option>
+                    <option value="Repositories" aria-placeholder="Inserisci una tipologia">Utenti</option>
+                </select>
+                <button class="btn btn-outline-success" type="submit">CERCA</button>
+            </div>
         </div>
     </nav>
 
