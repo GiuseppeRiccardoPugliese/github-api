73ed3b2fdf0a9923e76406596569c5d44673dfa9
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-04-09 21:26:10 +0200

    Milestone 2

--- src/components/ApiGitHub.vue
+++ src/components/ApiGitHub.vue
@@ -11,9 +11,12 @@ export default {
     },
     methods: {
         getRepo() {
-            const data = {
+            const selectedTypology = document.getElementById('seleziona').value; //Variabile per le options
+
+            let url;
+
+            let data = {
                 params: {
-                    // q: searchInput.value
                     q: this.searchInput,
                 },
                 headers: {
@@ -21,14 +24,19 @@ export default {
                     "X-GitHub-Api-Version": "2022-11-28"
                 }
             };
-            axios.get('https://api.github.com/search/repositories', data)
-                .then((res) => {
-                    // res.data.forEach(element => {
-                    //     this.repositories.push(element.name);
-                    // });
-                    // console.log(this.repositories[0]);
-                    this.repositories = res.data.items;
 
+            if (selectedTypology == 'repositories') {
+                url = 'https://api.github.com/search/repositories';
+            }
+            else if (selectedTypology == 'utenti_organizzazioni') {
+                url = 'https://api.github.com/search/users';
+            }
+            console.log(data);
+
+            axios.get(`${url}`, data)
+                .then((res) => {
+                    this.repositories = res.data.items;
+                    console.log(this.repositories);
                 })
                 .catch((error) => {
                     console.error("Error fetching data from first API:", error);
@@ -51,10 +59,10 @@ export default {
             <div class="search_input d-flex">
                 <input v-model="searchInput" class="form-control mx-1" type="search" placeholder="Search"
                     aria-label="Search">
-                <select class="mx-1" name="seleziona" id="">
-                    <option value="Seleziona" selected disabled>Seleziona un'opzione</option>
-                    <option value="Repositories" aria-placeholder="Inserisci una tipologia">Repositories</option>
-                    <option value="Repositories" aria-placeholder="Inserisci una tipologia">Utenti</option>
+                <select class="mx-1" name="seleziona" id="seleziona">
+                    <option disabled>Seleziona un'opzione</option>
+                    <option value="repositories" selected>Repositories</option>
+                    <option value="utenti_organizzazioni">Utenti/Organizzazioni</option>
                 </select>
                 <button @click="getRepo()" class="btn btn-outline-success" type="submit">CERCA</button>
             </div>
@@ -65,12 +73,13 @@ export default {
     <div class="container">
         <div class="row justify-content-center mt-4">
             <div v-for="repo in repositories" class="card m-2" style="width: 18rem; min-height: 350px;">
-                <img :src="repo.owner.avatar_url" class="card-img-top" alt="repo_img">
+                <img :src="repo.owner ? repo.owner.avatar_url : repo.avatar_url" class="card-img-top" alt="repo_img">
                 <div class="card-body h-100 d-flex flex-column">
-                    <h5 class="card-title">{{ repo.full_name }}</h5>
+                    <h5 class="card-title">{{ repo.full_name ? repo.full_name : repo.login }}</h5>
+                    <h6 v-if="repo.type"> Profilo: {{ repo.type == 'User' ? 'Utente' : 'Organizzazione' }}</h6>
                     <p class="card-text">{{ repo.description }}</p>
-                    <a :href="repo.html_url" class="btn btn-primary mt-auto">Vai al repo <i
-                            class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    <a :href="repo.html_url" class="btn btn-primary mt-auto">{{ repo.type ? 'Vai al profilo' :
+                        'Vai alla repo' }} <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
                 </div>
             </div>
         </div>
