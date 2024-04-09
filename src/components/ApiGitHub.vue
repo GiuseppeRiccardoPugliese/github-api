035965387c4fb9043b3b6a271aa04b5e7d600dff
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-04-09 19:52:57 +0200

    Fix chiamata api

--- src/components/ApiGitHub.vue
+++ src/components/ApiGitHub.vue
@@ -12,13 +12,23 @@ export default {
 
     },
     mounted() {
-        axios.get('https://api.github.com/repositories')
+        const data = {
+            params: {
+                // q: searchInput.value
+                q: 'vuejs'
+            },
+            headers: {
+                "Authorization": `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`,
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        };
+        axios.get('https://api.github.com/search/repositories', data)
             .then((res) => {
                 // res.data.forEach(element => {
                 //     this.repositories.push(element.name);
                 // });
                 // console.log(this.repositories[0]);
-                this.repositories = res.data;
+                this.repositories = res.data.items;
 
             })
             .catch((error) => {
@@ -34,9 +44,9 @@ export default {
     <div class="container">
         <div class="row justify-content-center mt-4">
             <div v-for="repo in repositories" class="card m-2" style="width: 18rem; min-height: 350px;">
-                <!-- <img src="..." class="card-img-top" alt="..."> -->
+                <img :src="repo.owner.avatar_url" class="card-img-top" alt="repo_img">
                 <div class="card-body h-100 d-flex flex-column">
-                    <h5 class="card-title">{{ repo.name }}</h5>
+                    <h5 class="card-title">{{ repo.full_name }}</h5>
                     <p class="card-text">{{ repo.description }}</p>
                     <a :href="repo.html_url" class="btn btn-primary mt-auto">Vai al repo <i
                             class="fa-solid fa-arrow-up-right-from-square"></i></a>
